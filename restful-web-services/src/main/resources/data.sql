cc18706e77aed258985a1f72552a0c3f14f2b807
IndiradeviRamakotti <indhuram97@gmail.com>
2021-07-02 15:29:09 +0530

    Todo-app

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'Indhu', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'Indhu', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'Indhu', 'Learn Microservices', sysdate(), false);
